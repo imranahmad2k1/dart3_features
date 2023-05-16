ff5d5b57c37a095e67a6d6fbbf7957903f473668
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 05:05:50 +0500

    learning dart3 patterns

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,28 +1,20 @@
 void main(List<String> arguments) {
-  final sameTypeList = sameType();
-  print(sameTypeList[0]);
-  print(sameTypeList[1]);
-
-  final diffTypeList = diffTypes();
-  print(diffTypeList[0]);
-  print(diffTypeList[1]);
-
   final diffTypeDart3List = diffTypesDart3();
   print(diffTypeDart3List.$1); //$1 for accessing first value
   print(diffTypeDart3List.$2); //$2 for accessing second value
+
+  final diffTypeDart3PatternsList = diffTypesDart3Patterns();
+  print(diffTypeDart3PatternsList
+      .stringsentence); //Accessing named parameter string value
+  print(diffTypeDart3PatternsList
+      .doublevalue); //Accessing named parameter double value
 }
 
-List<int> sameType() {
-  return [1, 2];
-}
-
-List<dynamic> diffTypes() {
-  //Problem is when you return List<dynamic> you don't know what data types you are returning
-  return ["This is a String", 999.00];
-}
-
-//BetterWay Dart3
-
+//BetterWay Dart3 for returning different data types
 (String, double) diffTypesDart3() {
   return ("My data type is Known", 10.0);
+}
+
+({String stringsentence, double doublevalue}) diffTypesDart3Patterns() {
+  return (stringsentence: "Advanced: My data type is Known", doublevalue: 50.0);
 }
