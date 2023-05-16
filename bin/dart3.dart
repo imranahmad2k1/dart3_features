a02908df6e8c71b605c38874cd7e97461d97dde6
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 17:39:21 +0500

    classes in dart3

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,15 +1,16 @@
-void main() {
-  final first = 1;
-  final second = 2;
+void main() {}
 
-  final position = second;
-  final bool competitor = true;
+sealed class Animal1 {} //just like abstract class, cant be initialized, switch case would require all cases for sealed class
 
-  final result = switch (position) {
-    1 when competitor => "You have gotten first Position",
-    2 when competitor => "You have gotten second Position",
-    _ => "Something wrong",
-  };
+final class Animal2 {} //can be initialized
 
-  print(result);
-}
+base class Animal3 {} //can only be extended not implemented
+
+interface class Animal4 {} //can only be implemented not extended, can also be constructed
+
+//combine with abstract and it will become true interface like other languages
+abstract interface class Animal5 {}
+
+mixin class Animal6 {} //can be used as a class and a mixin
+//in dart 2 you could use class Human5 with AnotherClass{} but can't with dart 3, you have to use mixin
+//this is the breaking change
