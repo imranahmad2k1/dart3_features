c416f126c34fe8d5db26fd136917162b680b9972
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 05:00:10 +0500

    learning dart3 records

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,3 +1,28 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  final sameTypeList = sameType();
+  print(sameTypeList[0]);
+  print(sameTypeList[1]);
+
+  final diffTypeList = diffTypes();
+  print(diffTypeList[0]);
+  print(diffTypeList[1]);
+
+  final diffTypeDart3List = diffTypesDart3();
+  print(diffTypeDart3List.$1); //$1 for accessing first value
+  print(diffTypeDart3List.$2); //$2 for accessing second value
+}
+
+List<int> sameType() {
+  return [1, 2];
+}
+
+List<dynamic> diffTypes() {
+  //Problem is when you return List<dynamic> you don't know what data types you are returning
+  return ["This is a String", 999.00];
+}
+
+//BetterWay Dart3
+
+(String, double) diffTypesDart3() {
+  return ("My data type is Known", 10.0);
 }
