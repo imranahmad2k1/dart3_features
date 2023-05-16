b4dd9a996d36a3e157b059b038a308113ad833bd
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 16:46:09 +0500

    items check in switch + OR statements + when statement + problem with when statement

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,7 +1,30 @@
-void main(List<String> arguments) {
-  var (a, b) = ("yes", "no");
-  print("$a $b");
-
-  (a, b) = (b, a);
-  print("$a $b");
+void main() {
+  List<dynamic> listOfItems = ["HI", 5];
+  int index = 5;
+  switch (listOfItems) {
+    //now you can check for list items and also can use OR and AND
+    case ["hello" || "HI", 10 || 5] when index == 5:
+      print("Hello Imran");
+    default:
+      print("Didn't work");
+  }
 }
+
+
+//Problem reported to Dart Github
+//Dart 3 when statement with switch doesn't work after continue newCase;
+//https://github.com/dart-lang/language/issues/3077
+// void main() {
+//   String greetings = "Hello";
+//   bool check = true;
+//   switch (greetings) {
+//     case "Hi" || "Hello" when check:
+//       print("Hello Imran");
+//       continue newcase;
+//     newcase:
+//     case "Hello" when check == false:
+//       print("Hello Hello");
+//     default:
+//       print("Didn't work");
+//   }
+// }
