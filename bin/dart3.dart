64b9026ee437df6acf4387c615ea06a6308d0ac9
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 17:26:30 +0500

    dart 3 switch for assignment

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,30 +1,15 @@
 void main() {
-  List<dynamic> listOfItems = ["HI", 5];
-  int index = 5;
-  switch (listOfItems) {
-    //now you can check for list items and also can use OR and AND
-    case ["hello" || "HI", 10 || 5] when index == 5:
-      print("Hello Imran");
-    default:
-      print("Didn't work");
-  }
+  final first = 1;
+  final second = 2;
+
+  final position = second;
+  final bool competitor = true;
+
+  final result = switch (position) {
+    1 when competitor => "You have gotten first Position",
+    2 when competitor => "You have gotten second Position",
+    _ => "Something wrong",
+  };
+
+  print(result);
 }
-
-
-//Problem reported to Dart Github
-//Dart 3 when statement with switch doesn't work after continue newCase;
-//https://github.com/dart-lang/language/issues/3077
-// void main() {
-//   String greetings = "Hello";
-//   bool check = true;
-//   switch (greetings) {
-//     case "Hi" || "Hello" when check:
-//       print("Hello Imran");
-//       continue newcase;
-//     newcase:
-//     case "Hello" when check == false:
-//       print("Hello Hello");
-//     default:
-//       print("Didn't work");
-//   }
-// }
