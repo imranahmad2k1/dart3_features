94d39872d8baadb2f2e20ee7376d63346ced1a94
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 15:42:27 +0500

    Dart 3, Errorless Switch and IF condition

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -11,4 +11,23 @@ void main(List<String> arguments) {
   //new
   final {'name': username, 'job': userjob} = jsoned;
   print("$username is a professional $userjob");
+
+  //above is error prone, to check and avoid errors
+  //if condition can't have final keyword, so we specify types
+  if (jsoned case {'name': String username, 'job': String userjob}) {
+    print("Errorless way: $username is a professional $userjob");
+  } else {
+    print("Incorrect JSON");
+  }
+
+  //same thing can be done with Switch
+
+  switch (jsoned) {
+    case {'name': String username, 'job': String userjob}:
+      print("$username is a professional $userjob");
+    //there is no break; when you write atleast one statement, dart puts break; by default in Dart 3
+    //but if you dont write any statement, dart won't put break;
+    default:
+      print("Incorrect JSON");
+  }
 }
