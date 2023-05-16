322a63c722df05e866b4eacb99b522e745353adb
Imran Ahmad <meetimrang@gmail.com>
2023-05-16 15:57:55 +0500

    Swapping using patterns

--- bin/dart3.dart
+++ bin/dart3.dart
@@ -1,26 +1,7 @@
 void main(List<String> arguments) {
-  final Human imran = Human("Imran Ahmad", 22);
-  final Human sayyab = Human("Ahmad Sayyab", 23);
-  final Human salman = Human("Salman Khan", 24);
+  var (a, b) = ("yes", "no");
+  print("$a $b");
 
-  //old approach
-  String fullNameOfImran = imran.name;
-  int ageOfImran = imran.age;
-
-  print("Full name: $fullNameOfImran \nAge: $ageOfImran years old\n");
-
-  //with patterns
-  final Human(:name, :age) = salman;
-  print("Full name: $name \nAge: $age years old\n");
-
-  //with patterns and variable names
-  final Human(name: fullNameOfSayyab, age: ageOfSayyab) = sayyab;
-  print("Full name: $fullNameOfSayyab \nAge: $ageOfSayyab years old");
-}
-
-class Human {
-  final String name;
-  final int age;
-
-  const Human(this.name, this.age);
+  (a, b) = (b, a);
+  print("$a $b");
 }
